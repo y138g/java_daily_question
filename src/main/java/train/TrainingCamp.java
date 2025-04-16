f5931445d227b31a14e90d539f5d7cca0d98eb80
y138g <BlueHorizon6005@gmail.com>
2025-04-16 16:38:31 +0800

    [LeetCodeTrainingCamp-2504160043] add：617：合并二叉树，前序遍历+递归

--- src/main/java/train/TrainingCamp.java
+++ src/main/java/train/TrainingCamp.java
@@ -1168,4 +1168,20 @@ public class TrainingCamp {
         root.right = constructMaximumBinaryTree(nums, maxIndex + 1, rightIndex);
         return root;
     }
+
+    /**
+     * 合并二叉树 实现方法1：递归 leetcode 617
+     *
+     * @param root1 二叉树1
+     * @param root2 二叉树2
+     * @return 合并后的二叉树
+     */
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
 }
