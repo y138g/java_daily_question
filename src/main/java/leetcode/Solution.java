35f1ae70f4b421a730075492cc9ae690a78e45f5
y138g <BlueHorizon6005@gmail.com>
2025-05-01 14:08:23 +0800

    [LeetCodeTrainingCamp-2505010088] add：63：不同路径II，动态规划

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -1171,5 +1171,28 @@ public class Solution {
         }
         return dp[m - 1][n - 1];
     }
+
+    /**
+     * 不同路径II 实现方法1：动态规划 leetcode 63
+     *
+     * @param obstacleGrid 二维数组
+     * @return 返回不同路径的数量
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) return 0;
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) dp[i][0] = 1;
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) dp[0][j] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
 
